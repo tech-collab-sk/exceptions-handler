b1fd38f89d5ac9963ea640dfd3df5c25ebddf604
Kamal Kag <kkag840@gmail.com>
2026-02-10 22:52:37 +0530

    Initial commit: project setup

---
+++ src/main/java/com/techcollab/exceptions/ExceptionsHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.techcollab.exceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionsHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionsHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techcollab/exceptions/ExceptionsHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techcollab.exceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionsHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
